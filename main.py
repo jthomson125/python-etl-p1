03d289e86bb9514b96ff508df78c16b76ea5147c
jthomson125 <jthomson125@gmail.com>
2021-12-07 23:03:21 -0600

    finished project

--- main.py
+++ main.py
@@ -1,13 +1,122 @@
 import sql
 from pgsql import query
+import csv
+
+
+#with open ('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/AnnualTicketSales.csv', newline='',
+    #encoding="utf-8-sig") as f:
+    #reader = csv.reader(f, delimiter=',')
+    #next(reader)
+    #for row in reader:
+        #row.pop()
+        #print(row)
+        #query(sql.create_insert, row)
+
+#with open ('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/HighestGrossers.csv', newline='',
+    #encoding="utf-8-sig") as f:
+    #reader = csv.reader(f, delimiter=',')
+    #next(reader)
+    #for row in reader:
+        #row.pop()
+        #print(row)
+        #query(sql.create_insert2, row)
+
+#with open ('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/PopularCreativeTypes.csv', newline='',
+    #encoding="utf-8-sig") as f:
+    #reader = csv.reader(f, delimiter=',')
+    #next(reader)
+    #for row in reader:
+        #row.pop()
+        #print(row)
+        #query(sql.create_insert3, row)
+
+#with open ('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/TopDistributors.csv', newline='',
+    #encoding="utf-8-sig") as f:
+    #reader = csv.reader(f, delimiter=',')
+    #next(reader)
+    #for row in reader:
+        #row.pop()
+        #print(row)
+        #query(sql.create_insert4, row)
+
+#with open ('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/TopGenres.csv', newline='',
+    #encoding="utf-8-sig") as f:
+    #reader = csv.reader(f, delimiter=',')
+    #next(reader)
+    #for row in reader:
+        #row.pop()
+        #print(row)
+        #query(sql.create_insert5, row)
+
+#with open ('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/TopGrossingRatings.csv', newline='',
+    #encoding="utf-8-sig") as f:
+    #reader = csv.reader(f, delimiter=',')
+    #next(reader)
+    #for row in reader:
+        #row.pop()
+        #print(row)
+        #query(sql.create_insert6, row)
+
+#with open ('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/TopGrossingSources.csv', newline='',
+    #encoding="utf-8-sig") as f:
+    #reader = csv.reader(f, delimiter=',')
+    #next(reader)
+    #for row in reader:
+        #row.pop()
+        #print(row)
+        #query(sql.create_insert7, row)
+
+#with open ('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/TopProductionMethods.csv', newline='',
+    #encoding="utf-8-sig") as f:
+    #reader = csv.reader(f, delimiter=',')
+    #next(reader)
+    #for row in reader:
+        #row.pop()
+        #print(row)
+        #query(sql.create_insert8, row)
+
+with open ('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/WideReleasesCount.csv', newline='',
+    encoding="utf-8-sig") as f:
+    reader = csv.reader(f, delimiter=',')
+    next(reader)
+    for row in reader:
+        #row.pop()
+        #print(row)
+        query(sql.create_insert9, row)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # insert data
-    query(sql.test_insert, ["My Insert!"]);
+    #query(sql.test_insert, ["My Insert!"]);
 
     # select data
-    results = query(sql.test_select);
-    print("results: ", results)
+    #results = query(sql.test_select);
+    #print("results: ", results)
+
+    # create schema
+    #query(sql.create_schema);
+
+    # create tables
+    #query(sql.create_table);
+    #query(sql.create_table2);
+    #query(sql.create_table3);
+    #query(sql.create_table4);
+    #query(sql.create_table5);
+    #query(sql.create_table6);
+    #query(sql.create_table7);
+    #query(sql.create_table8);
+    #query(sql.create_table9);
+
+    # insert data
+    #query(sql.create_insert);
+    #query(sql.create_insert2);
+    #query(sql.create_insert3);
+    #query(sql.create_insert4);
+    #query(sql.create_insert5);
+    #query(sql.create_insert6);
+    #query(sql.create_insert7);
+    #query(sql.create_insert8);
+    query(sql.create_insert9);
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- sql.py
+++ sql.py
@@ -1,9 +1,162 @@
 test_select = ('''
   SELECT *
-  FROM public.test_table;
+  FROM public.staff;
 ''')
 
 test_insert = ('''
   INSERT INTO public.test_table
   VALUES(%s);
+''')
+
+create_schema = ('''
+    CREATE SCHEMA IF NOT EXISTS petl1;
+''')
+
+create_table = ('''
+    CREATE TABLE IF NOT EXISTS petl1.annual_ticket_sales (
+        year TEXT PRIMARY KEY,
+        tickets_sold TEXT NOT NULL,
+        total_box_office TEXT NOT NULL,
+        total_inflation_adjusted_box_office TEXT NOT NULL,
+        average_ticket_price TEXT NOT NULL
+    );
+    
+''')
+
+create_table2 = ('''
+    CREATE TABLE IF NOT EXISTS petl1.highest_grossers (
+        year TEXT PRIMARY KEY,
+        movie TEXT NOT NULL,
+        genre TEXT NOT NULL,
+        mpaa_rating TEXT NOT NULL,
+        distributor TEXT NOT NULL,
+        total_for_year TEXT NOT NULL,
+        total_in_2019_dollars TEXT NOT NULL,
+        tickets_sold TEXT NOT NULL
+    );
+''')
+
+create_insert = ('''
+    INSERT INTO petl1.annual_ticket_sales
+    VALUES(%s, %s, %s, %s, %s);
+''')
+
+create_insert2 = ('''
+    INSERT INTO petl1.highest_grossers
+    VALUES(%s, %s, %s, %s, %s, %s, %s, %s);
+''')
+
+create_table3 = ('''
+    CREATE TABLE IF NOT EXISTS petl1.popular_creative_types (
+        rank TEXT PRIMARY KEY,
+        creative_types TEXT NOT NULL,
+        movies TEXT NOT NULL,
+        total_gross TEXT NOT NULL,
+        average_gross TEXT NOT NULL,
+        market_share TEXT NOT NULL
+    );
+''')
+
+create_insert3 = ('''
+    INSERT INTO petl1.popular_creative_types
+    VALUES(%s, %s, %s, %s, %s, %s);
+''')
+
+create_table4 = ('''
+    CREATE TABLE IF NOT EXISTS petl1.top_distributors (
+        rank TEXT PRIMARY KEY,
+        distributors TEXT NOT NULL,
+        movies TEXT NOT NULL,
+        total_gross TEXT NOT NULL,
+        average_gross TEXT NOT NULL,
+        market_share TEXT NOT NULL
+    );
+''')
+
+create_insert4 = ('''
+    INSERT INTO petl1.top_distributors
+    VALUES(%s, %s, %s, %s, %s, %s);
+''')
+
+create_table5 = ('''
+    CREATE TABLE IF NOT EXISTS petl1.top_genres (
+        rank TEXT PRIMARY KEY,
+        genres TEXT NOT NULL,
+        movies TEXT NOT NULL,
+        total_gross TEXT NOT NULL,
+        average_gross TEXT NOT NULL,
+        market_share TEXT NOT NULL
+    );
+''')
+
+create_insert5 = ('''
+    INSERT INTO petl1.top_genres
+    VALUES(%s, %s, %s, %s, %s, %s);
+''')
+
+create_table6 = ('''
+    CREATE TABLE IF NOT EXISTS petl1.top_grossing_ratings (
+        rank TEXT PRIMARY KEY,
+        mpaa_ratings TEXT NOT NULL,
+        movies TEXT NOT NULL,
+        total_gross TEXT NOT NULL,
+        average_gross TEXT NOT NULL,
+        market_share TEXT NOT NULL
+    );
+''')
+
+create_insert6 = ('''
+    INSERT INTO petl1.top_grossing_ratings
+    VALUES(%s, %s, %s, %s, %s, %s);
+''')
+
+create_table7 = ('''
+    CREATE TABLE IF NOT EXISTS petl1.top_grossing_sources (
+        rank TEXT PRIMARY KEY,
+        sources TEXT NOT NULL,
+        movies TEXT NOT NULL,
+        total_gross TEXT NOT NULL,
+        average_gross TEXT NOT NULL,
+        market_share TEXT NOT NULL
+    );
+''')
+
+create_insert7 = ('''
+    INSERT INTO petl1.top_grossing_sources
+    VALUES(%s, %s, %s, %s, %s, %s);
+''')
+
+create_table8 = ('''
+    CREATE TABLE IF NOT EXISTS petl1.top_production_methods (
+        rank TEXT PRIMARY KEY,
+        production_methods TEXT NOT NULL,
+        movies TEXT NOT NULL,
+        total_gross TEXT NOT NULL,
+        average_gross TEXT NOT NULL,
+        market_share TEXT NOT NULL
+    );
+''')
+
+create_insert8 = ('''
+    INSERT INTO petl1.top_production_methods
+    VALUES(%s, %s, %s, %s, %s, %s);
+''')
+
+create_table9 = ('''
+    CREATE TABLE IF NOT EXISTS petl1.wide_releases_count (
+        year TEXT PRIMARY KEY,
+        warner_bros TEXT NOT NULL,
+        walt_disney TEXT NOT NULL,
+        _20th_century_fox TEXT NOT NULL,
+        paramount_pictures TEXT NOT NULL,
+        sony_pictures TEXT NOT NULL,
+        universal TEXT NOT NULL,
+        total_major_6 TEXT NOT NULL,
+        total_other_studios TEXT NOT NULL
+    );
+''')
+
+create_insert9 = ('''
+    INSERT INTO petl1.wide_releases_count
+    VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s);
 ''')
